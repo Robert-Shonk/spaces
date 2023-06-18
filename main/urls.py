2aacfd816a63be09e3ecc3605af119c84eb1083b
Robert Shonk <rshonkcode@gmail.com>
2023-06-18 13:19:12 -0700

    Added links to login, signup. Added the .html's and routed them

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+
+app_name = "main"
 urlpatterns = [
     path('', views.index, name="index"),
+    path('login/', views.login_user, name='login_user'),
+    path('signup/', views.signup_user, name='signup_user'),
+    path('about/', views.about, name='about'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html', {})
 
+def login_user(request):
+    return HttpResponse("login page")
+
+def signup_user(request):
+    return HttpResponse("sign up page")
+
+def about(reqeust):
+    return HttpResponse("about us page")
+
